ac79fed995ac5737cced7d8cec40913f8b9b27d9
name <>
2024-05-03 11:20:48 +0100

    Fix variables not allowed

--- provider.tf
+++ provider.tf
@@ -2,18 +2,6 @@ variable "region" {
   type = string
 }
 
-variable "backend_bucket" {
-  type = string
-}
-
 provider "aws" {
   region = var.region
-}
-
-terraform {
-  backend "s3" {
-    bucket = var.backend_bucket
-    key    = "terraformstate"
-    region = var.region
-  }
 }
