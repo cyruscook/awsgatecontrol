a02478ab4471a66c7fbd8705d98333d744967661
name <>
2024-05-03 11:05:26 +0100

    Remove use of pip and python packages for lambda as no packages used

--- open_lambda_code/build.sh
+++ open_lambda_code/build.sh
@@ -4,13 +4,4 @@ cd "$(dirname "$0")"
 
 rm -f lambda_deployment.zip
 
-rm -rf ./package
-mkdir -p ./package
-
-pip install --target ./package -r requirements.txt
-
-cd package
-zip -r ../lambda_deployment.zip .
-
-cd ..
 zip lambda_deployment.zip lambda_function.py
